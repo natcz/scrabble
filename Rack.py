ee8c0b75552a4ffc782075d9090d8778d1875bfb
natcz <58185832+natcz@users.noreply.github.com>
2020-05-24 22:29:14 +0200

    Add files via upload

--- Rack.py
+++ Rack.py
@@ -1,22 +1,38 @@
-from Letters import *
+from Sack import *
 from random import shuffle
 class Rack:
 
-    def __intit__(self):
-        self.rack=[]
-        self.intitRack()
-        self.rack.shuffle()
+    def __init__(self, sack,max_l):
+        self.rack = []
+        self.sack = sack
+        self.max_letters = max_l
+        self.initRack()
 
 
     def initRack(self):
-        for elem in Letters:
-            for _ in range(elem[1]):
-                self.rack.add((elem,elem[0]))
+        for _ in range(self.max_letters):
+            if self.sack.left_letters() != 0:
+                self.rack.append(self.sack.take_letter())
+        self.sack.shuffle()
 
-    def left_letters(self):
-        return len(self.rack)
 
-    def take_letter(self):
-        return  self.rack.pop()
+
+    def fillRack(self):
+        while len(self.rack) < self.max_letters and  self.sack.left_letters() > 0:
+                self.rack.append(self.sack.take_letter())
+
+
+    def exchangeAll(self):
+        for letter in self.rack:
+            self.sack.append(letter)
+            self.rack.remove(letter)
+        self.initRack()
+
+    def exchangeOne(self, indx):
+        letter = self.rack[indx]
+        self.sack.append(letter)
+        del self.rack[indx]
+        self.rack.append(self.sack.take_letter())
+
 
 
