6eb8625aac041959127d41bd7a05fc1274d8743a
natcz <58185832+natcz@users.noreply.github.com>
2020-06-16 21:02:09 +0200

    newgame->names->board

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from BoardVisual import *
+from Board import *
 from StartGame import *
 from Sack import *
-
+from Player import *
 from BoardVisual import *
+
 class Main:
     startframe: Frame
 
@@ -12,20 +14,24 @@ class Main:
         self.startframe = Frame(self.root)
         self.mainframe = Frame(self.root)
         self.sack = Sack()
-        self.startgame = StartGame(self.sack,"pl1", "pl2")
+        self.player1 = Player(self.sack,7,"")
+        self.player2 = Player(self.sack,7,"")
+        self.startgame = StartGame()
 
         self.root.title("Scrabble")
         self.root.geometry("1500x900")
         self.startframe.grid(column = 0, row = 0)
         startLabel = Label(self.startframe, text = "Welcome to Scrabble!")
         startLabel.grid(column = 3, row = 0)
-        startButton = Button(self.startframe, text = "New game", command = print("clicked!"))
+        startButton = Button(self.startframe, text = "New game", command = lambda: Main.getNames(self))
         startButton.grid(column = 3, row = 5)
         instrButton = Button(self.startframe, text = "See the instruction", command = lambda: Main.get_instr(self))
         instrButton.grid(column = 3, row = 8)
         self.root.mainloop()
 
-
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 
     def get_instr(self):
         instrWindow =  Toplevel()
@@ -36,8 +42,29 @@ class Main:
         closeinstrButton.grid(column = 3, row = 3)
 
 
-    def getPlayersNames(self):
-        self.name1entry = Entry(self.startframe, validate="key", validatecommand=(vcmd, '%P'))
+    def getNames(self):
+        nameWindow = Toplevel()
+        name1entry = Entry(nameWindow)
+        name1Label = Label(nameWindow, text = "Player1")
+        name2entry = Entry(nameWindow)
+        name2Label = Label(nameWindow, text="Player2")
+        confirmB = Button(nameWindow, text="Confirm" , command = lambda : [saveName(self),nameWindow.destroy(), self.startframe.destroy(),Main.letsPlay(self)])
+
+        name1entry.grid(column = 2,  row = 1)
+        name2entry.grid(column = 2, row = 3)
+        name1Label.grid(column=1, row=1)
+        name2Label.grid(column=1, row=3)
+        confirmB.grid(column = 3, row = 3)
+
+        def saveName(self):
+            self.player1.name = name1entry.get()
+            self.player2.name = name2entry.get()
+
+    def letsPlay(self):
+        b = [['' for columns in range(15)] for rows in range(15)]
+        board = BoardVisual(self.root,self.mainframe, b)
+        
+
 
 
 Main()
