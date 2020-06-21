0e711455eed3415fdee5abf59e70744bddb05ceb
natcz <58185832+natcz@users.noreply.github.com>
2020-06-21 22:33:52 +0200

    changed hint function

--- Hint.py
+++ Hint.py
@@ -13,7 +13,6 @@ class Hint:
 
     def similarLetters(self, word1, word2):
 
-
         w1 = dd(lambda: 0)
         w2 = dd(lambda: 0)
         counter = 0
@@ -21,10 +20,7 @@ class Hint:
             w1[letter] += 1
         for letter in word2:
             w2[letter] += 1
-        print(w1)
-        print(w2)
         for key, val in w1.items():
-            print(val,w2[key])
             counter += min(val, w2[key])
         return counter
 
@@ -74,11 +70,10 @@ class Hint:
 
     def findHint(self):
         list_eng_dict = list(self.eng_dict)
+        list_eng_dict = [word for word in list_eng_dict if len(word) <= 6 and len(word) >= 2]
         list_eng_dict.sort(key=lambda x: self.similarLetters(x, self.rack), reverse=True)
-        list_eng_dict = [word for word in list_eng_dict if len(word) <= 5 and len(word) >= 2]
-        print(len(list_eng_dict))
-        print("wooop")
-        board_letters_coords = dd()
+
+        board_letters_coords = dd(list)
         s = len(self.board.board)
 
         for i in range(s):
@@ -88,9 +83,9 @@ class Hint:
                     down = self.freeDown(i, j)
                     right = self.freeRight(i, j)
                     left = self.freeLeft(i, j)
-                    board_letters_coords[self.board.board[i][j]] = [i, j, up, down, right, left]
-        print("wooop2")
-        print(board_letters_coords)
+                    board_letters_coords[self.board.board[i][j]].append([i, j, up, down, right, left])
+
+
 
         for word in list_eng_dict:
             w = set(word)
@@ -98,55 +93,50 @@ class Hint:
                 if letter in w:
                     rack = self.rack[:]
                     rack.append(letter)
-                    print(rack)
-
-
-                    print("ohno")
-                    print(word)
-                    print(self.similarLetters(word,rack))
                     if self.similarLetters(word,rack) < len(word):
                         continue
                     else:
-                        print("hello")
                         ind = word.index(letter)
                         l_left = ind
                         r_left = len(word) - ind - 1
-                        free_up = board_letters_coords[letter][2]
-                        free_down = board_letters_coords[letter][3]
-                        free_right = board_letters_coords[letter][4]
-                        free_left = board_letters_coords[letter][5]
-                        x = board_letters_coords[letter][0]
-                        y = board_letters_coords[letter][1]
-                        horizon_good = l_left == free_left and r_left == free_right
-                        vertic_good = l_left == free_up and r_left == free_down
-                        w_coords = []
-                        print(word)
-                        if horizon_good or vertic_good:
-                            print("uh")
-                            if horizon_good:
-                                print("wow")
-                                print(x,y)
-                                i = 0
-                                j = ind
-                                while i < len(word):
-                                    w_coords.append((x,y-j))
-                                    j -= 1
-                                    i += 1
+                        for i in range(len(board_letters_coords[letter])):
+                            free_up = board_letters_coords[letter][i][2]
+                            free_down = board_letters_coords[letter][i][3]
+                            free_right = board_letters_coords[letter][i][4]
+                            free_left = board_letters_coords[letter][i][5]
+                            x = board_letters_coords[letter][i][0]
+                            y = board_letters_coords[letter][i][1]
+                            print(word)
+                            print(letter)
+                            print(l_left)
+                            print(r_left)
+                            horizon_good = l_left <= free_left and r_left <= free_right
+                            vertic_good = l_left <= free_up and r_left <= free_down
+                            w_coords = []
+                            if horizon_good or vertic_good:
+                                
 
-                                if CheckBoard(self.board).checkBoard(self.eng_dict, word, w_coords):
-                                    return word, w_coords
+                                if horizon_good:
+                                    i = 0
+                                    j = ind
+                                    while i < len(word):
+                                        w_coords.append((x,y-j))
+                                        j -= 1
+                                        i += 1
 
-                            if vertic_good:
-                                print("wow")
-                                i = 0
-                                j = ind
-                                while i < len(word):
-                                    w_coords.append((x-j, y))
-                                    j -= 1
-                                    i += 1
+                                    if CheckBoard(self.board).checkBoard(self.eng_dict, word, w_coords):
+                                        return word, w_coords
 
-                                if CheckBoard(self.board).checkBoard(self.eng_dict, word, w_coords):
-                                    return word, w_coords
-                        else:
-                            continue
+                                if vertic_good:
+                                    i = 0
+                                    j = ind
+                                    while i < len(word):
+                                        w_coords.append((x-j, y))
+                                        j -= 1
+                                        i += 1
+
+                                    if CheckBoard(self.board).checkBoard(self.eng_dict, word, w_coords):
+                                        return word, w_coords
+                            else:
+                                continue
         return "", []
